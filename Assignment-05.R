43676478a56ccb40315a3e3894f6efee9ff98b63
Adam Stavash <ajs223@lehigh.edu>
2019-11-15 14:42:25 -0500

    Maybe added code

--- Assignment-05.R
+++ Assignment-05.R
@@ -1,1 +1,11 @@
-data.table::fread("PEAAUS00000A647NCEN.csv")
+ptm <- proc.time()
+DF <- read.csv("PEAAUS00000A647NCEN.csv")
+CSV_READ_TIME <- (proc.time() - ptm)
+CSV_READ_TIME
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+ptm <- proc.time()
+DF <- fread("PEAAUS00000A647NCEN.csv", header="auto", 
+            data.table=FALSE)
+FREAD_READ_TIME <- (proc.time() - ptm)
+FREAD_READ_TIME
